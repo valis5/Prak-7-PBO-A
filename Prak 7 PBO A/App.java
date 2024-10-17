c6b9c1d39d158ed24552953d5c8c7b8814ffabca
valis5 <palis4523012@univpancasila.ac.id>
2024-10-17 14:50:32 +0700

    Add files via upload

---
+++ Prak 7 PBO A/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Luar l = new Luar();
+        Luar.Dalam d = l.new Dalam();
+
+        d.bicara();
+    }
+}
